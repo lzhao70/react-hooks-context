3f6879a2f0be43b84fdab69373f0a129208240a7
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-08 02:56:43 -0500

    add pages

---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='about-container'>
+      <p className='about-p'>Author: Lucio Zhao</p>
+      <p className='about-p'>React App - Github Finder</p>
+      <p className='about-p'>Version: 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='not-found-container'>
+      <p>404 - Page not found.</p>
+    </div>
+  );
+};
+
+export default About;
