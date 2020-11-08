780d3bc558becbd19c31b6a87ea865e06f5e20f9
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-07 23:40:42 -0500

    resolve use effect issue

--- src/Components/Users/UserDetails.js
+++ src/Components/Users/UserDetails.js
@@ -1,35 +1,36 @@
 import React, { useEffect } from 'react';
 
 const UserDetails = (props) => {
+  const { getUser, user } = props;
   useEffect(() => {
-    props.getUser(props.match.params.login);
-  });
+    getUser(props.match.params.login);
+  }, [getUser, props.match.params.login]);
 
   return (
     <div className='user-card detail-card'>
       <div className='detail-card-row'>
         <label htmlFor=''>Login</label>
-        <input type='text' value={this.props.user.login} disabled />
+        <input type='text' value={user.login} disabled />
       </div>
 
       <div className='detail-card-row'>
         <label htmlFor=''>Full Name</label>
-        <input type='text' value={this.props.user.name} disabled />
+        <input type='text' value={user.name} disabled />
       </div>
 
       <div className='detail-card-row'>
         <label htmlFor=''>Location</label>
-        <input type='text' value={this.props.user.location} disabled />
+        <input type='text' value={user.location} disabled />
       </div>
 
       <div className='detail-card-row'>
         <label htmlFor=''>Followers</label>
-        <input type='text' value={this.props.user.followers} disabled />
+        <input type='text' value={user.followers} disabled />
       </div>
 
       <div className='detail-card-row'>
         <label htmlFor=''>Following</label>
-        <input type='text' value={this.props.user.following} disabled />
+        <input type='text' value={user.following} disabled />
       </div>
     </div>
   );
