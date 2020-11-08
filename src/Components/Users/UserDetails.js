60fe274b7543bf66ee4c39f6be7e57aa1121486c
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-07 23:28:18 -0500

    introduction to use effect lifecycle for hooks

--- src/Components/Users/UserDetails.js
+++ src/Components/Users/UserDetails.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class UserDetails extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-  }
+const UserDetails = (props) => {
+  useEffect(() => {
+    props.getUser(props.match.params.login);
+  });
 
-  render() {
-    return (
-      <div className='user-card detail-card'>
-        <div className='detail-card-row'>
-          <label htmlFor=''>Login</label>
-          <input type='text' value={this.props.user.login} disabled />
-        </div>
-
-        <div className='detail-card-row'>
-          <label htmlFor=''>Full Name</label>
-          <input type='text' value={this.props.user.name} disabled />
-        </div>
-
-        <div className='detail-card-row'>
-          <label htmlFor=''>Location</label>
-          <input type='text' value={this.props.user.location} disabled />
-        </div>
-
-        <div className='detail-card-row'>
-          <label htmlFor=''>Followers</label>
-          <input type='text' value={this.props.user.followers} disabled />
-        </div>
-
-        <div className='detail-card-row'>
-          <label htmlFor=''>Following</label>
-          <input type='text' value={this.props.user.following} disabled />
-        </div>
+  return (
+    <div className='user-card detail-card'>
+      <div className='detail-card-row'>
+        <label htmlFor=''>Login</label>
+        <input type='text' value={this.props.user.login} disabled />
       </div>
-    );
-  }
-}
+
+      <div className='detail-card-row'>
+        <label htmlFor=''>Full Name</label>
+        <input type='text' value={this.props.user.name} disabled />
+      </div>
+
+      <div className='detail-card-row'>
+        <label htmlFor=''>Location</label>
+        <input type='text' value={this.props.user.location} disabled />
+      </div>
+
+      <div className='detail-card-row'>
+        <label htmlFor=''>Followers</label>
+        <input type='text' value={this.props.user.followers} disabled />
+      </div>
+
+      <div className='detail-card-row'>
+        <label htmlFor=''>Following</label>
+        <input type='text' value={this.props.user.following} disabled />
+      </div>
+    </div>
+  );
+};
 
 export default UserDetails;
